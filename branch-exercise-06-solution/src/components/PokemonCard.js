a6e57a802db9591634b599b187812753769271ce
Nilan <nilan.marktanner@student.kit.edu>
2016-11-03 17:14:27 +0100

    add 06 solution

---
+++ branch-exercise-06-solution/src/components/PokemonCard.js
@@ -0,0 +1,95 @@
+import React from 'react'
+import Fragment from 'graphql-fragments'
+import gql from 'graphql-tag'
+import { graphql } from 'react-apollo'
+
+class PokemonCard extends React.Component {
+
+  static fragments = {
+    pokemon: new Fragment(gql`
+      fragment PokemonCardPokemon on Pokemon {
+        id
+        imageUrl
+        name
+      }
+    `)
+  }
+
+  static propTypes = {
+    pokemon: PokemonCard.fragments.pokemon.propType,
+    handleCancel: React.PropTypes.func.isRequired,
+    afterUpdate: React.PropTypes.func.isRequired,
+  }
+
+  state = {
+    name: this.props.pokemon.name,
+    imageUrl: this.props.pokemon.imageUrl,
+  }
+
+  render () {
+    return (
+      <div className='w-100 pa4 flex justify-center'>
+        <div style={{ maxWidth: 400 }} className=''>
+          <input
+            className='w-100 pa3 mv2'
+            value={this.state.name}
+            placeholder='Name'
+            onChange={(e) => this.setState({name: e.target.value})}
+          />
+          <input
+            className='w-100 pa3 mv2'
+            value={this.state.imageUrl}
+            placeholder='Image Url'
+            onChange={(e) => this.setState({imageUrl: e.target.value})}
+          />
+          {this.state.imageUrl &&
+            <img src={this.state.imageUrl} role='presentation' className='w-100 mv3' />
+          }
+          <div className='flex justify-between'>
+            <button className='pa3 bn dim ttu bg-red pointer' onClick={this.handleDelete}>Delete</button>
+            <button className='pa3 bn dim ttu pointer' onClick={this.props.handleCancel}>Cancel</button>
+            {this.canUpdate()
+              ? <button className='pa3 bn dim ttu bg-dark-green pointer' onClick={this.handleCancel}>Update</button>
+              : <button className='pa3 bn ttu gray light-gray'>Update</button>
+            }
+          </div>
+        </div>
+      </div>
+    )
+  }
+
+  canUpdate = () => {
+    return this.state.name && this.state.imageUrl &&
+      (this.props.pokemon.name !== this.state.name || this.props.pokemon.imageUrl !== this.state.imageUrl)
+  }
+
+  handleCancel = () => {
+    this.props.updatePokemon({variables: {id: this.props.pokemon.id, name: this.state.name, imageUrl: this.state.imageUrl}})
+      .then(this.props.afterUpdate)
+  }
+}
+
+const updatePokemon = gql`
+  mutation updatePokemon($id: ID!, $name: String!, $imageUrl: String!) {
+    updatePokemon(id: $id, name: $name, imageUrl: $imageUrl) {
+      id
+      name
+      imageUrl
+    }
+  }
+`
+
+const deletePokemon = gql`
+  mutation deletePokemon($id: ID!) {
+    deletePokemon(id: $id) {
+      id
+    }
+  }
+`
+
+const PokemonCardWithMutations =  graphql(deletePokemon, {name : 'deletePokemon'})(
+  graphql(updatePokemon, {name: 'updatePokemon'})(PokemonCard)
+)
+
+
+export default PokemonCardWithMutations
---
+++ branch-exercise-06-solution/src/components/PokemonPage.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import { withRouter } from 'react-router'
+import { graphql } from 'react-apollo'
+import gql from 'graphql-tag'
+
+import PokemonCard from './PokemonCard'
+import PokemonCardHeader from './PokemonCardHeader'
+
+class PokemonPage extends React.Component {
+
+  static propTypes = {
+    data: React.PropTypes.object.isRequired,
+    router: React.PropTypes.object.isRequired,
+    params: React.PropTypes.object.isRequired,
+  }
+
+  render () {
+    if (this.props.data.loading) {
+      return (<div>Loading</div>)
+    }
+
+    const pokemon = this.props.data.Pokemon
+
+    return (
+      <div>
+        <PokemonCardHeader pokemon={PokemonCardHeader.fragments.pokemon.filter(pokemon)} />
+        <PokemonCard
+          pokemon={PokemonCard.fragments.pokemon.filter(pokemon)}
+          handleCancel={this.goBack}
+          afterUpdate={this.goBack} />
+      </div>
+    )
+  }
+
+  goBack = () => {
+    this.props.router.replace('/')
+  }
+}
+
+const PokemonQuery = gql`query($id: ID!) {
+    Pokemon(id: $id) {
+      ... PokemonCardPokemon
+      ... PokemonCardHeaderPokemon
+    }
+  }
+`
+
+const PokemonPageWithData = graphql(PokemonQuery, {
+  options: (ownProps) => ({
+      variables: {
+        id: ownProps.params.pokemonId
+      },
+      fragments: [PokemonCardHeader.fragments.pokemon.fragments(), PokemonCard.fragments.pokemon.fragments()]
+    })
+  }
+)(withRouter(PokemonPage))
+
+export default PokemonPageWithData
