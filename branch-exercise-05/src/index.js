5d7b8eaa7670ea949745ad1c3c3f39b8f956f50f
Nilan <nilan.marktanner@student.kit.edu>
2016-11-03 13:22:55 +0100

    add AddPokemonCard route and remove dataIdFromObject from 05

--- branch-exercise-05/src/index.js
+++ branch-exercise-05/src/index.js
@@ -2,16 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Pokedex from './components/Pokedex'
 import PokemonPage from './components/PokemonPage'
+import AddPokemonCard from './components/AddPokemonCard'
 import { Router, Route, browserHistory } from 'react-router'
 import ApolloClient, { createNetworkInterface } from 'apollo-client'
 import { ApolloProvider } from 'react-apollo'
 import 'tachyons'
 import './index.css'
 
-// set dataIdFromObject for normalization! otherwise different components might be out-of-sync
+
 const client = new ApolloClient({
   networkInterface: createNetworkInterface({ uri: 'https://api.graph.cool/simple/v1/ciuwgdxhn25po0151dv2imjad'}),
-  dataIdFromObject: o => o.id
 })
 
 ReactDOM.render((
@@ -19,6 +19,7 @@ ReactDOM.render((
     <Router history={browserHistory}>
       <Route path='/' component={Pokedex} />
       <Route path='/view/:pokemonId' component={PokemonPage} />
+      <Route path='/create/:trainerId' component={AddPokemonCard} />
     </Router>
   </ApolloProvider>
   ),
