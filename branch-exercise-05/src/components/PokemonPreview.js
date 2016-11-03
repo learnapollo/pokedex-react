38c9160f2b55f951c18869b74fde5883ed5447d9
Nilan <nilan.marktanner@student.kit.edu>
2016-11-03 13:44:58 +0100

    fix pokemon preview for 05

--- branch-exercise-05/src/components/PokemonPreview.js
+++ branch-exercise-05/src/components/PokemonPreview.js
@@ -9,11 +9,13 @@ export default class PokemonPreview extends React.Component {
 
   render () {
     return (
-      <Link to={`/view/${this.props.pokemon.id}`} className='dib mw4 tc black link dim ml1 mr1 mb2 bg-white pa2'>
-        <div className='db'>
-          <img src={this.props.pokemon.imageUrl} alt={this.props.pokemon.name} />
-        </div>
-        <span className='gray'>{this.props.pokemon.name}</span>
+      <Link
+        to={`/view/${this.props.pokemon.id}`}
+        style={{ minWidth: 200 }}
+        className='link dim grow mw4 bg-white ma2 pa3 shadow-1'
+      >
+        <img src={this.props.pokemon.imageUrl} alt={this.props.pokemon.name} />
+        <div className='gray tc'>{this.props.pokemon.name}</div>
       </Link>
     )
   }
