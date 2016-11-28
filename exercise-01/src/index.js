daa4aad73cad59a405ecb8be124b72c31bc4c60d
Nilan <nilan.marktanner@student.kit.edu>
2016-11-28 11:57:23 +0100

    split first exercise in React only and introducing Apollo steps

--- exercise-01/src/index.js
+++ exercise-01/src/index.js
@@ -2,21 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Pokedex from './components/Pokedex'
 import { Router, Route, browserHistory } from 'react-router'
-import ApolloClient, { createNetworkInterface } from 'apollo-client'
-import { ApolloProvider } from 'react-apollo'
 import 'tachyons'
 import './index.css'
 
-const client = new ApolloClient({
-  networkInterface: createNetworkInterface({ uri: 'https://api.graph.cool/simple/v1/__PROJECT_ID__'}),
-})
-
 ReactDOM.render((
-  <ApolloProvider client={client}>
-    <Router history={browserHistory}>
-      <Route path='/' component={Pokedex} />
-    </Router>
-  </ApolloProvider>
+  <Router history={browserHistory}>
+    <Route path='/' component={Pokedex} />
+  </Router>
   ),
   document.getElementById('root')
 )
