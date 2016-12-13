c0f28e81b8400f1a93e5b77be2a7163d1e700183
Nilan <nilan.marktanner@student.kit.edu>
2016-12-13 17:39:42 +0100

    prepare hosted demo

---
+++ hosted-demo/src/components/PokemonPage.js
@@ -0,0 +1,69 @@
+import React from 'react'
+import { withRouter } from 'react-router'
+import { graphql } from 'react-apollo'
+import gql from 'graphql-tag'
+import { filter } from 'graphql-anywhere'
+
+import PokemonCard from './PokemonCard'
+import PokemonCardHeader from './PokemonCardHeader'
+
+class PokemonPage extends React.Component {
+
+  static propTypes = {
+    data: React.PropTypes.shape({
+      loading: React.PropTypes.bool,
+      error: React.PropTypes.object,
+      Pokemon: React.PropTypes.object,
+    }).isRequired,
+    router: React.PropTypes.object.isRequired,
+    params: React.PropTypes.object.isRequired,
+  }
+
+  render () {
+    if (this.props.data.loading) {
+      return (<div>Loading</div>)
+    }
+
+    if (this.props.data.error) {
+      console.log(this.props.data.error)
+      return (<div>An unexpexted error occured</div>)
+    }
+
+    const pokemon = this.props.data.Pokemon
+
+    return (
+      <div>
+        <PokemonCardHeader pokemon={filter(PokemonCardHeader.fragments.pokemon, pokemon)} />
+        <PokemonCard
+          pokemon={filter(PokemonCard.fragments.pokemon, pokemon)}
+          handleCancel={this.goBack}
+          afterChange={this.goBack} />
+      </div>
+    )
+  }
+
+  goBack = () => {
+    this.props.router.replace('/')
+  }
+}
+
+const PokemonQuery = gql`query PokemonQuery($id: ID!) {
+    Pokemon(id: $id) {
+      ... PokemonCardPokemon
+      ... PokemonCardHeaderPokemon
+    }
+  }
+  ${PokemonCardHeader.fragments.pokemon}
+  ${PokemonCard.fragments.pokemon}
+`
+
+const PokemonPageWithData = graphql(PokemonQuery, {
+    options: (ownProps) => ({
+      variables: {
+        id: ownProps.params.pokemonId
+      }
+    })
+  }
+)(withRouter(PokemonPage))
+
+export default PokemonPageWithData
---
+++ hosted-demo/src/index.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Pokedex from './components/Pokedex'
+import PokemonPage from './components/PokemonPage'
+import AddPokemonCard from './components/AddPokemonCard'
+import { Router, Route, browserHistory, IndexRedirect } from 'react-router'
+import ApolloClient, { createNetworkInterface } from 'apollo-client'
+import { ApolloProvider } from 'react-apollo'
+import 'tachyons'
+import './index.css'
+
+
+const client = new ApolloClient({
+  networkInterface: createNetworkInterface({ uri: 'https://api.graph.cool/simple/v1/ciwjew0qz0l8d0122v7smvmxu'}),
+  dataIdFromObject: o => o.id
+})
+
+ReactDOM.render((
+  <ApolloProvider client={client}>
+    <Router history={browserHistory}>
+      <Route path='/' component={Pokedex}>
+        <IndexRedirect to='/1' />
+        <Route path='/:page' component={Pokedex} />
+      </Route>
+      <Route path='/view/:pokemonId' component={PokemonPage} />
+      <Route path='/create/:trainerId' component={AddPokemonCard} />
+    </Router>
+  </ApolloProvider>
+  ),
+  document.getElementById('root')
+)
